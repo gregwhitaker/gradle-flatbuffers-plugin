b6c565f70f154a22f7db1d8b27c78a34df7f78ed
gregwhitaker <gwhitake@gmail.com>
2017-01-25 22:07:30 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+gradle-flatbuffers-plugin
+===
---
+++ src/main/groovy/com/github/gregwhitaker/flatbuffers/plugin/FlatBuffersPlugin.groovy
@@ -0,0 +1,14 @@
+package com.github.gregwhitaker.flatbuffers.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class FlatBuffersPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+    
+}
