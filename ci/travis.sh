97b5f2bb3e87efa530e23ff8e62737aaef6e1462
gregwhitaker <gwhitake@gmail.com>
2018-11-21 16:26:55 -0800

    Added travis.sh build process

---
+++ ci/travis.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This script builds the project in Travis
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
+  ./gradlew clean build
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  ./gradlew clean build
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew clean build publishPlugins
+else
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  ./gradlew clean build
+fi
